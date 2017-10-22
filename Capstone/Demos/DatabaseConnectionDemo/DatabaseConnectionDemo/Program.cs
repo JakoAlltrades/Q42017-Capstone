e63f495004c51ff8854e62ff54add3670d21f12e
JakoAlltrades <Jpriemo@gmail.com>
2017-10-22 13:45:27 -0600

    Initial Commit of Code
    This contains a few demos and the start of my application

---
+++ Capstone/Demos/DatabaseConnectionDemo/DatabaseConnectionDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace DatabaseConnectionDemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            MainAsync().Wait();
+
+            Console.ReadLine();
+        }
+
+        static async Task MainAsync()
+        {
+            var connectionString = "mongodb://10.10.19.42:27017";//possible IP for VMWare
+            //var client = new MongoClient(new MongoUrl("mongodb://localhost:27017"));
+            var client = new MongoClient(connectionString);
+        }
+    }
+}
---
+++ Capstone/Model_Plan/CapstoneModel/CapstoneModelLib/GeneratedCode/Address.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapstoneModelLib.GeneratedCode
+{
+    public class Address
+    {
+        public string StreetAddress { get; set; }
+        public string City { get; set; }
+        public int? ApartmentNumber { get; set; }
+        public int Zipcode { get; set; }
+
+        public string State { get; set; }
+
+        public Address(string streetAddress, string city, string state, int zipcode, int? aptNum = null)
+        {
+            StreetAddress = streetAddress;
+            City = city;
+            State = state;
+            Zipcode = zipcode;
+            ApartmentNumber = aptNum;
+        }
+    }
+}
