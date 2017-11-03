b008029b7177d7036fb8345816fb9e10cff3fe8c
JakoAlltrades <Jpriemo@gmail.com>
2017-11-02 18:39:00 -0600

    Added test for Sign in and create to the library classes

---
+++ Capstone/Model_Plan/CapstoneModel/LibraryTest/UserActionTests.cs
@@ -0,0 +1,20 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LibraryTest
+{
+    [TestClass]
+    public class UserActionTests
+    {
+        [TestMethod]
+        public void SignInTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void CreateAccountTest()
+        {
+
+        }
+    }
+}
