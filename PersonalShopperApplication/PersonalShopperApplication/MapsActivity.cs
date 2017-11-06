6b38ac9af0993c1edd141696365787eb74c37a41
JakoAlltrades <Jpriemo@gmail.com>
2017-11-06 00:12:23 -0700

    Added Maps to Project
    The maps demo I had was in a seperate project

---
+++ PersonalShopperApplication/PersonalShopperApplication/MapsActivity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Gms.Maps;
+
+namespace PersonalShopperApplication.Resources.layout
+{
+    [Activity(Label = "MapsActivity")]
+    public class MapsActivity : Activity, IOnMapReadyCallback
+    {
+        public void OnMapReady(GoogleMap googleMap)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+        }
+    }
+}
--- PersonalShopperApplication/PersonalShopperApplication/Resources/Resource.Designer.cs
+++ PersonalShopperApplication/PersonalShopperApplication/Resources/Resource.Designer.cs
@@ -44,92 +44,98 @@ namespace PersonalShopperApplication
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f040018
-			public const int Password = 2130968600;
-			
-			// aapt resource value: 0x7f040017
-			public const int Username = 2130968599;
-			
-			// aapt resource value: 0x7f04000b
-			public const int button1 = 2130968587;
+			// aapt resource value: 0x7f04001a
+			public const int Password = 2130968602;
 			
 			// aapt resource value: 0x7f040019
-			public const int button2 = 2130968601;
-			
-			// aapt resource value: 0x7f04001b
-			public const int button3 = 2130968603;
-			
-			// aapt resource value: 0x7f04001c
-			public const int button4 = 2130968604;
-			
-			// aapt resource value: 0x7f040016
-			public const int creApt = 2130968598;
-			
-			// aapt resource value: 0x7f040013
-			public const int creCity = 2130968595;
-			
-			// aapt resource value: 0x7f040010
-			public const int creConPass = 2130968592;
-			
-			// aapt resource value: 0x7f04000c
-			public const int creFirstName = 2130968588;
+			public const int Username = 2130968601;
 			
 			// aapt resource value: 0x7f04000d
-			public const int creLastName = 2130968589;
+			public const int button1 = 2130968589;
 			
-			// aapt resource value: 0x7f04000f
-			public const int crePassword = 2130968591;
+			// aapt resource value: 0x7f04001b
+			public const int button2 = 2130968603;
+			
+			// aapt resource value: 0x7f04001d
+			public const int button3 = 2130968605;
+			
+			// aapt resource value: 0x7f04001e
+			public const int button4 = 2130968606;
+			
+			// aapt resource value: 0x7f040018
+			public const int creApt = 2130968600;
 			
 			// aapt resource value: 0x7f040015
-			public const int creStates = 2130968597;
+			public const int creCity = 2130968597;
 			
 			// aapt resource value: 0x7f040012
-			public const int creStreetAdd = 2130968594;
+			public const int creConPass = 2130968594;
 			
 			// aapt resource value: 0x7f04000e
-			public const int creUsername = 2130968590;
+			public const int creFirstName = 2130968590;
 			
-			// aapt resource value: 0x7f040014
-			public const int creZip = 2130968596;
-			
-			// aapt resource value: 0x7f040002
-			public const int isShopper = 2130968578;
+			// aapt resource value: 0x7f04000f
+			public const int creLastName = 2130968591;
 			
 			// aapt resource value: 0x7f040011
-			public const int linearLayout1 = 2130968593;
+			public const int crePassword = 2130968593;
 			
-			// aapt resource value: 0x7f040003
-			public const int listView1 = 2130968579;
+			// aapt resource value: 0x7f040017
+			public const int creStates = 2130968599;
 			
-			// aapt resource value: 0x7f04001a
-			public const int orderedItems = 2130968602;
+			// aapt resource value: 0x7f040014
+			public const int creStreetAdd = 2130968596;
 			
-			// aapt resource value: 0x7f040001
-			public const int space1 = 2130968577;
+			// aapt resource value: 0x7f040010
+			public const int creUsername = 2130968592;
+			
+			// aapt resource value: 0x7f040016
+			public const int creZip = 2130968598;
 			
 			// aapt resource value: 0x7f040004
-			public const int space2 = 2130968580;
+			public const int isShopper = 2130968580;
 			
-			// aapt resource value: 0x7f040006
-			public const int space3 = 2130968582;
-			
-			// aapt resource value: 0x7f040008
-			public const int space4 = 2130968584;
-			
-			// aapt resource value: 0x7f04000a
-			public const int space5 = 2130968586;
-			
-			// aapt resource value: 0x7f040000
-			public const int textView1 = 2130968576;
+			// aapt resource value: 0x7f040013
+			public const int linearLayout1 = 2130968595;
 			
 			// aapt resource value: 0x7f040005
-			public const int textView2 = 2130968581;
+			public const int listView1 = 2130968581;
+			
+			// aapt resource value: 0x7f040001
+			public const int map = 2130968577;
+			
+			// aapt resource value: 0x7f040000
+			public const int mapcontainer = 2130968576;
+			
+			// aapt resource value: 0x7f04001c
+			public const int orderedItems = 2130968604;
+			
+			// aapt resource value: 0x7f040003
+			public const int space1 = 2130968579;
+			
+			// aapt resource value: 0x7f040006
+			public const int space2 = 2130968582;
+			
+			// aapt resource value: 0x7f040008
+			public const int space3 = 2130968584;
+			
+			// aapt resource value: 0x7f04000a
+			public const int space4 = 2130968586;
+			
+			// aapt resource value: 0x7f04000c
+			public const int space5 = 2130968588;
+			
+			// aapt resource value: 0x7f040002
+			public const int textView1 = 2130968578;
 			
 			// aapt resource value: 0x7f040007
-			public const int textView3 = 2130968583;
+			public const int textView2 = 2130968583;
 			
 			// aapt resource value: 0x7f040009
-			public const int textView4 = 2130968585;
+			public const int textView3 = 2130968585;
+			
+			// aapt resource value: 0x7f04000b
+			public const int textView4 = 2130968587;
 			
 			static Id()
 			{
@@ -145,37 +151,40 @@ namespace PersonalShopperApplication
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int BecomeAShopper = 2130837504;
+			public const int BaseMaps = 2130837504;
 			
 			// aapt resource value: 0x7f020001
-			public const int Checkout = 2130837505;
+			public const int BecomeAShopper = 2130837505;
 			
 			// aapt resource value: 0x7f020002
-			public const int CreateAccount1 = 2130837506;
+			public const int Checkout = 2130837506;
 			
 			// aapt resource value: 0x7f020003
-			public const int CreateAccount2 = 2130837507;
+			public const int CreateAccount1 = 2130837507;
 			
 			// aapt resource value: 0x7f020004
-			public const int Home = 2130837508;
+			public const int CreateAccount2 = 2130837508;
 			
 			// aapt resource value: 0x7f020005
-			public const int Main = 2130837509;
+			public const int Home = 2130837509;
 			
 			// aapt resource value: 0x7f020006
-			public const int PlaceOrder = 2130837510;
+			public const int Main = 2130837510;
 			
 			// aapt resource value: 0x7f020007
-			public const int PreviousDeliveries = 2130837511;
+			public const int PlaceOrder = 2130837511;
 			
 			// aapt resource value: 0x7f020008
-			public const int PreviousOrders = 2130837512;
+			public const int PreviousDeliveries = 2130837512;
 			
 			// aapt resource value: 0x7f020009
-			public const int ReceiveOrder = 2130837513;
+			public const int PreviousOrders = 2130837513;
 			
 			// aapt resource value: 0x7f02000a
-			public const int Settings = 2130837514;
+			public const int ReceiveOrder = 2130837514;
+			
+			// aapt resource value: 0x7f02000b
+			public const int Settings = 2130837515;
 			
 			static Layout()
 			{
